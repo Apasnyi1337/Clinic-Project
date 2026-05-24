8c951d9c886a7f8235701bb468f2a57b5a43caee
ararasnyi <cassidy.ren2033@gmail.com>
2026-05-24 17:47:43 +0300

    Initial commit: Django project base

---
+++ clinic/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def main_page(request):
+    return HttpResponse("<h1>Привет! Это главная страница нашей ветклиники!</h1>")
